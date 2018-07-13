a8a43eee309a8d1e0ce2e8192c6703c0ff1a71bb
Diego Hernandez Nater <dhernandeznater@gmail.com>
2018-07-13 08:56:47 -0500

    Add files via upload

--- Palindrome Tester (HTML)/Palindrome Tester (HTML)/Palindrome_Tester.js
+++ Palindrome Tester (HTML)/Palindrome Tester (HTML)/Palindrome_Tester.js
@@ -1,6 +1,29 @@
 let inputWord = document.querySelector("#testWord");
-document.addEventListener("keydown", () =>
-{
-  console.log(inputWord.value);
 
+
+var testButton = document.querySelector("#Test");
+
+
+function testWord()
+{
+  var testWord = inputWord.value;
+  testWord = testWord.replace(/[^a-zA-Z-]/g, "");
+  let displayWord = testWord;
+  testWord = testWord.toLowerCase();
+  var testArray = testWord.split("");
+  var reverseArray = testWord.split("");
+  reverseArray = reverseArray.reverse();
+  var reverseWord = reverseArray.join("");
+  console.log(reverseWord);
+  if(testWord == reverseWord)
+    alert(displayWord + " is a palindrome!");
+  else
+    alert(displayWord + " is not a palindrome")
+
+
+}
+
+testButton.addEventListener("click", ()=>
+{
+  testWord();
 })
